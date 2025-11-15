2d643f3f57f1891bb2176b6b3412c52a5a04060c
THe_FøX <aristeidisstagkos21203@gmail.com>
2025-11-15 23:06:21 +0200

    Update Javascript.js

--- Js/Javascript.js
+++ Js/Javascript.js
@@ -1,11 +1,12 @@
 let angle = 0;
-  const h2 = document.getElementById("img.icon");
+  const h2 = document.getElementById("icon");
 
   setInterval(() => {
     angle += 3; // degrees per frame
     h2.style.transform = `rotate(${angle}deg)`;
 
   }, 16);
+
 
 
 
