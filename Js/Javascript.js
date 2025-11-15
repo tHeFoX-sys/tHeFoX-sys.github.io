a2759c4b1fd62601fe748ce63e56250996ab8ed9
THe_FøX <aristeidisstagkos21203@gmail.com>
2025-11-15 23:05:04 +0200

    Update Javascript.js

--- Js/Javascript.js
+++ Js/Javascript.js
@@ -1,10 +1,11 @@
 let angle = 0;
-  const h2 = document.getElementById("hacked");
+  const h2 = document.getElementById("img.icon");
 
   setInterval(() => {
     angle += 3; // degrees per frame
     h2.style.transform = `rotate(${angle}deg)`;
 
   }, 16);
+
 
 
