661d33b537dd52383266e512182d07dcf0c06ec4
THe_FøX <aristeidisstagkos21203@gmail.com>
2025-11-16 12:04:16 +0200

    Update Javascript.js

--- Js/Javascript.js
+++ Js/Javascript.js
@@ -1,11 +1,12 @@
 let angle = 0;
-  const h2 = document.getElementById("iconic");
+  const h2 = document.getElementById("GIF/Haacker.gif");
 
   setInterval(() => {
     angle += 3; // degrees per frame
     h2.style.transform = `rotate(${angle}deg)`;
 
   }, 16);
+
 
 
 
