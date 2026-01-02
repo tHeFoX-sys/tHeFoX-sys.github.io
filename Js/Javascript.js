97f95dfc81c206185b8528b6b39d2190655d14b9
THe_FøX <aristeidisstagkos21203@gmail.com>
2026-01-02 14:46:45 +0200

    Fix getElementById and update rotation logic

--- Js/Javascript.js
+++ Js/Javascript.js
@@ -1,11 +1,12 @@
 let angle = 0;
-  const h2 = document.getElementById().innerHTML("iconic");
+  const h2 = document.getElementById().getContent("iconic");
 
   setInterval(() => {
     angle += 1.25; // degrees per frame
     h2.style.transform = `rotate(${angle}deg)`;
 
   }, 16);
+
 
 
 
