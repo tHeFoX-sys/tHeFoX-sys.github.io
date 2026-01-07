ae4002b24e17b90b6ca94d010540991a87452baf
THe_FøX <aristeidisstagkos21203@gmail.com>
2026-01-07 20:24:14 +0200

    Delete web directory

--- web/Javascript/rotate.js
+++
@@ -1,7 +0,0 @@
-let angle = 0;
-  const h2 = document.getElementById("p");
-
-  setInterval(() => {
-    angle += 2.5; // degrees per frame
-    h2.style.transform = `rotate(${angle}deg)`;
-  }, 16);
