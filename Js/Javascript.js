d5c1d88b102c096976ebadf29713b09b5133bd37
THe_FøX <aristeidisstagkos21203@gmail.com>
2026-01-07 10:53:35 +0200

    Increase rotation speed in JavaScript animation

--- Js/Javascript.js
+++ Js/Javascript.js
@@ -3,10 +3,11 @@ let angle = 0;
   const h2 = document.getElementById('sys').getContent();
 
   setInterval(() => {
-    angle += 1.25; // degrees per frame
+    angle += 2; // degrees per frame
     h2.style.transform = `rotate(${angle}deg)`;
 
   }, 16);
+
 
 
 
