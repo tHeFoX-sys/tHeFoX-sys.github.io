6f9046a2a17aec96b20bac74fe2db5dfb3569eba
THe_FøX <aristeidisstagkos21203@gmail.com>
2026-01-07 10:56:17 +0200

    Fix getContent() call to retrieve h2 element

--- Js/Javascript.js
+++ Js/Javascript.js
@@ -1,12 +1,13 @@
 let angle = 0;
 
-  const h2 = document.getElementById('sys').getContent();
+  const h2 = document.getElementById('sys');
 
   setInterval(() => {
     angle += 2; // degrees per frame
     h2.style.transform = `rotate(${angle}deg)`;
 
   }, 16);
+
 
 
 
