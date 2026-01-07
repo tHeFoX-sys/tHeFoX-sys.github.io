1bc918727c57c11d42e3c3404027f28307c25a0e
THe_FøX <aristeidisstagkos21203@gmail.com>
2026-01-07 13:17:17 +0200

    Update myscript.js

--- js/myscript.js
+++ js/myscript.js
@@ -2,10 +2,11 @@ let angle = 0;
   const h2 = document.getElementById("h4.rotate#r");
 
   setInterval(() => {
-    while(1===1){
+    while(true){
     angle += 2; // degrees per frame
     h2.style.transform = `rotate(${angle}deg)`;
     }
   }, 16);
+
 
 
